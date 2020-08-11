bc72a7974c0386255aee85e0b32417526f4876c3
evyatar peretz <atatar1521@gmail.com>
2020-08-11 19:13:52 +0300

    Init commit v1.1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
-   useNewUrlParser : true,
+mongoose.connect(process.env.MONGODB_URL, {
+   useNewUrlParser: true,
    useCreateIndex: true,
-   useFindAndModify:false,
-   useUnifiedTopology: true 
-})
-
+   useFindAndModify: false,
+   useUnifiedTopology: true
+  })
 
